765ddba423c9b57e91c03a6976ebfaaf1e7318f1
meganpekarek <megan@livefront.com>
2025-01-03 16:37:17 -0600

    get mixins to show on the front end

--- code.ts
+++ code.ts
@@ -1,12 +1,3 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
 type FontData = {
@@ -27,22 +18,18 @@ type FontMixin = {
   desktopLetterSpacing?: string;
 };
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = (msg: { type: string; count: number }) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === "create-shapes") {
+/** Generates sass mixins for all font data. */
+figma.ui.onmessage = (msg: { type: string }) => {
+  if (msg.type === "create-sass-mixins") {
     const parentFrame = figma.currentPage.selection[0] as FrameNode;
-    console.log(parentFrame);
     const fontData = getFontData(parentFrame, "All Styles");
     const fontMixins = fontDataAsMixins(fontData);
-    console.log(fontMixins);
+
+    figma.ui.resize(800, 800);
+    figma.showUI(fontMixins);
+    // console.log(fontMixins);
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
   // figma.closePlugin();
 };
 
